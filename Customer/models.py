55425a8d3a3602edccddf9d6baa258507631e560
Husnain Ahmad <husnainahmad521@gmail.com>
2020-09-05 21:23:10 +0500

    creating apps models

--- Customer/models.py
+++ Customer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+class Customer(models.Model):
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
+
+
+    def __str__(self):
+        return self.first_name
+
+
+    def get_absolute_url(self):
+        """Return absolute url for Student."""
+        return ('')    
+
+    
--- src/settings.py
+++ src/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Customer',
+    'Authentication',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'Authentication.User'
