082d265f08e539c7c8055ef603393aec30456855
Husnain Ahmad <husnainahmad521@gmail.com>
2020-09-06 22:43:54 +0500

    Login and logout

--- Customer/views.py
+++ Customer/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render , HttpResponse , redirect
-from Authentication.models import *
+from django.shortcuts import render , HttpResponse , redirect , Http404 
+from Authentication.models import User ,UserManager
+from .models import Customer
+from django.contrib.auth import authenticate , login , logout
 from .forms import CustomerRegistrationForm
-def home(request):
-    context = {}
-    return render(request,"home.html",context)
+
+def signin(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=email, password=password)
+        print(user.user_type)
+        
+        if user is not None :            
+            login(request,user)
+            if user.user_type == "CUSTOMER":
+                customer =  Customer.objects.get(user__email=user.email)
+                return redirect('customer-page' , customer.id)
+            else:
+                pass               
+        else:
+            pass
+    return render(request , 'signin.html')
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
+def customerpage(request,pk):
+    try:
+        customer =  Customer.objects.get(id=pk)
+    except User.DoesNotExist:
+        raise Http404("User Does Not Exisit")
+
+    context = {'customer':customer}
+    return render(request,"customer.html",context)
 
 
 def register(request):
