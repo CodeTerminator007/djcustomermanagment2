a69da88a656dd07eb302a3b58a1ea7a4abaa236a
Husnain Ahmad <husnainahmad521@gmail.com>
2020-09-15 12:41:09 +0500

    permissions and urls

--- Customer/urls.py
+++ Customer/urls.py
@@ -4,12 +4,10 @@ from .views import *
 from Authentication.views import signin , signout
 
 urlpatterns = [
-    path('<str:pk>/',customer_home ,name="customer-home"),
-    path("order/<str:pk>/",place_order ,name="place_order"),
-    path('update_order/<str:pk>' , update_order ,name='updae_order'),
+    path('',customer_home ,name="customer-home"),
+    path("order/",place_order ,name="place_order"),
     path('delete_order/<str:pk>',delete_order, name="delete_order"),
-    path('settings_user/<str:pk>',settings_user, name="settings_user"),
-
-    
+    path('settings_user/',settings_user, name="settings_user"),
+    path('products/',customer_products_view, name="customer_products"),
 
 ]
